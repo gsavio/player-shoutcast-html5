34e6ddd60bffc8d728d0689a05030ee63afe8c5a
Guilherme Sávio <guilhermesavio96@gmail.com>
2020-06-21 19:13:15 -0300

    change streaming url

--- js/script.js
+++ js/script.js
@@ -22,10 +22,10 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const RADIO_NAME = "Rock FM";
+const RADIO_NAME = "Brasil Hits";
 
 // URL of SHOUTCast streaming without / on the final, eg: http://streaming.com:8080
-const URL_STREAMING = "http://104.156.244.180:8484";
+const URL_STREAMING = "http://streaming.brasilhits.com:8050";
 
 // Visit https://api.vagalume.com.br/docs/ yo get your API key
 const API_KEY = "18fe07917957c289983464588aabddfb";
