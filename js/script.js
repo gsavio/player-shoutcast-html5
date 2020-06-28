d1d6f9fccc6cee46145efbd50692b14b13703db0
Guilherme Sávio <guilhermesavio96@gmail.com>
2020-06-28 05:07:33 -0300

    update demo api

--- js/script.js
+++ js/script.js
@@ -381,7 +381,7 @@ function getStreamingData() {
     var d = new Date();
 
     // Requisition with timestamp to prevent cache on mobile devices
-    xhttp.open('GET', 'api.php?url=' + URL_STREAMING + '&streamtype=' + STREAMING_TYPE + '&historic=' + HISTORIC + '&next=' + NEXT_SONG + '&t=' + d.getTime(), true);
+    xhttp.open('GET', 'https://guilhermesavio.dev/api.php?url=' + URL_STREAMING + '&streamtype=' + STREAMING_TYPE + '&historic=' + HISTORIC + '&next=' + NEXT_SONG + '&t=' + d.getTime(), true);
     xhttp.send();
 }
 
